71fa3c89587c932b9ca7b938d4e659b8c4c6ddd1
Rafael Otero <josmanmx@hotmail.com>
2019-04-12 15:55:04 -0400

    added structure for portfolio

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -1,7 +1,18 @@
 <template>
   <section class="section">
     <h2 class="section-title">{{title}}</h2>
-    <p class="section-text">Datos Personales</p>
+    <div class="section-content">
+      <div class="portfolios">
+        <div v-for="portfolio in portfolios" :key="portfolio.key" class="portfolio">
+          <a :href="portfolio.url" target="_blank">
+            <span class="portfolio-description">{{portfolio.description}}</span>
+            <div class="portfolio-image">
+              <img :src="portfolios" :alt="portfolio.description">
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -10,11 +21,46 @@ export default {
   name: "introduction",
   data() {
     return {
-      title: "Portafolio"
+      title: "Portafolio",
+      portfolios: [
+        {
+          description: "En Behance",
+          url: "https://www.behance.net/jostechs",
+          // FIXME no carga las imagenes como debería
+          image: "../assets/avatar-j-rafael-otero.jpg"
+        },
+        {
+          description: "En JosTechS",
+          url: "https://jostech.com.ve/portafolio",
+          image: "/assets/portfolio/jostech-isotipo.svg"
+        }
+      ]
     };
   }
 };
 </script>
 
 <style lang="scss">
+.portfolio {
+  &s {
+    display: flex;
+    justify-content: space-between;
+  }
+  & {
+    text-align: center;
+    a {
+      color: var(--brown);
+    }
+  }
+  &-image {
+    margin-top: 10px;
+    width: 350px;
+    border-radius: 4px;
+    padding: 0.25em;
+    border: 1px solid var(--brown-light);
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
